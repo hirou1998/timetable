11d5fc939473414a747625dbcbad810b96653582
hirou1998 <tanahi1025@gmail.com>
2020-06-21 02:34:15 +0900

    enable to add course

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Course;
+use App\User;
 
 class CourseController extends Controller
 {
@@ -16,6 +17,19 @@ class CourseController extends Controller
             'type' => $request->type
         ]);
         return $course->updatePeriod($course, $request);
+    }
+
+    public function store(User $user, Request $request)
+    {
+        $new_course = $user->courses()->create([
+            'name' => $request->name,
+            'teacher' => $request->teacher,
+            'type' => $request->type,
+        ]);
+
+        $new_course->updatePeriod($new_course, $request);
+
+        return $new_course;
     }
 
 }
