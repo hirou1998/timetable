863cb91a93750828717612e836a5558fae0327ef
hirou1998 <tanahi1025@gmail.com>
2020-08-19 03:12:32 +0900

    enabled change period-setting

--- resources/js/components/Period-setting.vue
+++ resources/js/components/Period-setting.vue
@@ -11,7 +11,7 @@
 				</template>
 				<template v-else>
 					<button class="btn btn-sm btn-danger" @click="toggleEditing">取消</button>
-					<button class="btn btn-sm btn-success">保存</button>
+					<button class="btn btn-sm btn-success" @click="changePeriod">保存</button>
 				</template>
 			</div>
 		</div>
@@ -46,18 +46,41 @@ export default {
         },
     },
     methods: {
-        getSettingInfo(){
-            axios.get(`/api/user/setting/${this.auth.id}`)
-            .then(({data}) => {
-                this.periods = data.periods;
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-        },
-        toggleEditing(){
-            this.isEditing = !this.isEditing
-        }
+			changePeriod(){
+				var formData = {
+					first_start_time: this.periods[0][0],
+					first_end_time: this.periods[0][1],
+					second_start_time: this.periods[1][0],
+					second_end_time: this.periods[1][1],
+					third_start_time: this.periods[2][0],
+					third_end_time: this.periods[2][1],
+					fourth_start_time: this.periods[3][0],
+					fourth_end_time: this.periods[3][1],
+					fifth_start_time: this.periods[4][0],
+					fifth_end_time: this.periods[4][1],
+					sixth_start_time: this.periods[5][0],
+					sixth_end_time: this.periods[5][1]
+				}
+				axios.put(`/setting/period/${this.auth.id}`, formData)
+				.then((res) => {
+					this.isEditing = false
+				})
+				.catch((err) => {
+					console.log(err)
+				})
+			},
+			getSettingInfo(){
+					axios.get(`/api/user/setting/${this.auth.id}`)
+					.then(({data}) => {
+							this.periods = data.periods;
+					})
+					.catch((err) => {
+							console.log(err);
+					})
+			},
+			toggleEditing(){
+					this.isEditing = !this.isEditing
+			}
     },
     mounted(){
         this.getSettingInfo();
