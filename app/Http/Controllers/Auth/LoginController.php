e732973c0fe51d08206b73e169030378960a4b3b
hirou1998 <tanahi1025@gmail.com>
2020-09-14 03:18:31 +0900

    changed login/register page design and started craeting mypage

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -10,24 +10,9 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
 
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
     protected $redirectTo = '/timetable';
 
     protected function authenticated(Request $request)
@@ -41,11 +26,6 @@ class LoginController extends Controller
         session()->put('api_token', $token);
     }
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
--- resources/js/components/Top.vue
+++ resources/js/components/Top.vue
@@ -1,5 +1,20 @@
 <template>
-    <div>top</div>
+    <main class="main-container">
+        <div class="top-container">
+            <div class="top-image">
+                <img src="/images/top.png" alt="">
+            </div>
+            <h1 class="top-title">時間割</h1>
+            <div class="top-button-container">
+                <router-link to="/register">
+                    <button class="top-button top-signup">登録</button>
+                </router-link>
+                <router-link to="/login">
+                    <button class="top-button top-login">ログイン</button>
+                </router-link>
+            </div>
+        </div>
+    </main>
 </template>
 
 <script>
